58b53b5feeecb2dda909e0f5dc1454e70a92136f
Dheena R S <dheenacsgroup@gmail.com>
2026-03-23 16:02:17 +0530

    add

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -57,16 +57,19 @@ const releaseSeatsAndDeleteBooking=inngest.createFunction(
         const tenMinutesLater=new Date(Date.now()+10*60*1000);
         await step.sleepUntil('wait-for-10-minutes',tenMinutesLater);
         await step.run('check-payment-status',async()=>{
+            await connectDB();
             const bookingId=event.data.bookingId;
             const booking =await Booking.findById(bookingId)
 
-            if(!booking.isPaid){
+            if(booking && !booking.isPaid){
                 const show = await Show.findById(booking.show);
-                booking.bookedSeats.forEach((seat)=>{
-                    delete show.occupiedSeats[seat]
-                });
-                show.markModified('occupiedSeats')
-                await show.save()
+                if (show) {
+                    booking.bookedSeats.forEach((seat)=>{
+                        delete show.occupiedSeats[seat]
+                    });
+                    show.markModified('occupiedSeats')
+                    await show.save()
+                }
                 await Booking.findByIdAndDelete(booking._id)
             }
         })
